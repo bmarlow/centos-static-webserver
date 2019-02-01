268b649a4c9d5d889cb85cf146b50fd0f5464426
unknown <brandon.n.marlow@gmail.com>
2019-02-01 14:06:56 -0800

    updating format

---
+++ create_index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat <<EOF >> /var/run/web/index.html
+<html>
+  <head>
+    <title>My Test app</title>
+  </head>
+  <body>
+    <h1>Hello World</h1>
+    <p>This is my local application</p>
+    <p>Version 1.2</p>
+    <p>Hostname of container is $(hostname)</p>
+    <p>This container was started at $(date)</p>
+  </body>
+</html>
+EOF
